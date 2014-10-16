0a6096448aef0a8e9eeedb29c28ee25a8b1370e0
Peter J. Radics <peter.radics@jutility.org>
2014-10-15 22:57:24 -0400

    Cleaned up code.

--- src/main/java/org/jutility/io/database/Prototype.java
+++ src/main/java/org/jutility/io/database/Prototype.java
@@ -1,22 +1,22 @@
 package org.jutility.io.database;
 
+
 /*
- * #%L
- * jutility-incubation
- * %%
- * Copyright (C) 2013 - 2014 jutility.org
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * #%L 
+ * jutility-incubation 
+ * %% 
+ * Copyright (C) 2013 - 2014 jutility.org 
+ * %% Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. 
  * #L%
  */
 
@@ -70,54 +70,116 @@ public class Prototype<T> {
         return Collections.unmodifiableMap(this.primaryKeyValueMap);
     }
 
+    /**
+     * Returns the prototype of the provided element property.
+     * 
+     * @param elementProperty
+     *            the element property.
+     * @return the element property prototype.
+     */
     public Prototype<?> getElementPrototype(PropertyInfo elementProperty) {
 
         return this.elementPrototypes.get(elementProperty);
     }
 
+    /**
+     * Returns the element properties.
+     * 
+     * @return the element properties.
+     */
     public Collection<PropertyInfo> getElementProperties() {
 
         return this.elementPrototypes.keySet();
     }
 
+    /**
+     * Returns the element property prototypes.
+     * 
+     * @return the element property prototypes.
+     */
     public Map<PropertyInfo, Prototype<?>> getElementPrototypes() {
 
         return Collections.unmodifiableMap(this.elementPrototypes);
     }
 
+    /**
+     * Adds an element property prototype.
+     * 
+     * @param elementProperty
+     *            the element property.
+     * @param elementPrototype
+     *            the element prototype.
+     * @return the previous element prototype.
+     */
     public Prototype<?> addElementPrototype(PropertyInfo elementProperty,
             Prototype<?> elementPrototype) {
 
         return this.elementPrototypes.put(elementProperty, elementPrototype);
     }
 
+    /**
+     * Removes an element property prototype.
+     * 
+     * @param elementProperty
+     *            the element property.
+     * @return the removed element prototype.
+     */
     public Prototype<?> removeElementPrototye(PropertyInfo elementProperty) {
 
         return this.elementPrototypes.remove(elementProperty);
     }
 
+    /**
+     * Clears the element prototype map.
+     */
     public void clearElementPrototypes() {
 
         this.elementPrototypes.clear();
     }
 
 
+    /**
+     * Returns the prototype list for the element list property.
+     * 
+     * @param elementListProperty
+     *            the element list property.
+     * @return the prototype list for the element list property.
+     */
     public List<Prototype<?>> getElementListPrototypes(
             ListPropertyInfo elementListProperty) {
 
         return this.elementListPrototypes.get(elementListProperty);
     }
 
+    /**
+     * Returns the element list properties.
+     * 
+     * @return the element list properties.
+     */
     public Collection<ListPropertyInfo> getElementListProperties() {
 
         return this.elementListPrototypes.keySet();
     }
 
+    /**
+     * Returns the element list property prototypes.
+     * 
+     * @return the element list property prototypes.
+     */
     public Map<ListPropertyInfo, List<Prototype<?>>> getElementListPrototypes() {
 
         return Collections.unmodifiableMap(this.elementListPrototypes);
     }
 
+    /**
+     * Adds element list property prototypes.
+     * 
+     * @param elementListProperty
+     *            the element list property.
+     * @param elementListPrototypes
+     *            the element list prototype.
+     * @return the previous element prototypes.
+     */
     public List<Prototype<?>> addElementListPrototypes(
             ListPropertyInfo elementListProperty,
             List<Prototype<?>> elementListPrototypes) {
@@ -126,18 +188,34 @@ public class Prototype<T> {
                 elementListPrototypes);
     }
 
+    /**
+     * Removes element list property prototypes.
+     * 
+     * @param elementListProperty
+     *            the element list property.
+     * @return the removed element prototypes.
+     */
     public List<Prototype<?>> removeElementListPrototyes(
             ListPropertyInfo elementListProperty) {
 
         return this.elementListPrototypes.remove(elementListProperty);
     }
 
+    /**
+     * Clears the element list prototypes.
+     */
     public void clearElementListPrototypes() {
 
         this.elementListPrototypes.clear();
     }
 
 
+    /**
+     * Sets the reference prototype.
+     * 
+     * @param referencePrototype
+     *            the reference prototype.
+     */
     public void setReferencePrototype(Prototype<?> referencePrototype) {
 
 
@@ -146,6 +224,12 @@ public class Prototype<T> {
 
 
 
+    /**
+     * Returns whether or not this is a referenced prototype.
+     * 
+     * @return {@code true} if this is a referenced prototype; {@code false}
+     *         otherwise.
+     */
     public boolean isReferencePrototype() {
 
         return this.referencePrototype == null;
--- src/main/java/org/jutility/io/database/mysql/MySQLDriver.java
+++ src/main/java/org/jutility/io/database/mysql/MySQLDriver.java
@@ -1,23 +1,19 @@
 package org.jutility.io.database.mysql;
 
+
 /*
- * #%L
- * jutility-incubation
- * %%
- * Copyright (C) 2013 - 2014 jutility.org
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * #%L jutility-incubation %% Copyright (C) 2013 - 2014 jutility.org %% Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. #L%
  */
 
 
@@ -29,18 +25,13 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.jutility.io.database.DBUtils;
 import org.jutility.io.database.ISQLPrivileges;
-import org.jutility.io.database.PropertyInfo;
 
 
 /**
@@ -215,6 +206,11 @@ public class MySQLDriver {
         }
     }
 
+    /**
+     * @param query
+     * @return the number of query results.
+     * @throws SQLException
+     */
     public int determineNumberOfQueryResults(String query)
             throws SQLException {
 
@@ -300,6 +296,17 @@ public class MySQLDriver {
     }
 
 
+    /**
+     * Creates a user.
+     * 
+     * @param username
+     *            the user name.
+     * @param hostname
+     *            the database host name.
+     * @param password
+     *            the password.
+     * @throws SQLException
+     */
     public void createUser(final String username, final String hostname,
             final String password)
             throws SQLException {
@@ -332,6 +339,15 @@ public class MySQLDriver {
     }
 
 
+    /**
+     * Drops a user.
+     * 
+     * @param username
+     *            the user name.
+     * @param hostname
+     *            the database host name.
+     * @throws SQLException
+     */
     public void dropUser(final String username, final String hostname)
             throws SQLException {
 
@@ -360,6 +376,21 @@ public class MySQLDriver {
         }
     }
 
+    /**
+     * Grants privileges on an object to a user.
+     * 
+     * @param privileges
+     *            the privileges to grant.
+     * @param object
+     *            the object.
+     * @param username
+     *            the user name.
+     * @param hostname
+     *            the database host name.
+     * @param password
+     *            the password.
+     * @throws SQLException
+     */
     public void grantPrivileges(final Set<ISQLPrivileges> privileges,
             final String object, final String username, final String hostname,
             final String password)
@@ -406,6 +437,19 @@ public class MySQLDriver {
         }
     }
 
+    /**
+     * Revokes privileges on an object of a user.
+     * 
+     * @param privileges
+     *            the privileges to grant.
+     * @param object
+     *            the object.
+     * @param username
+     *            the user name.
+     * @param hostname
+     *            the database host name.
+     * @throws SQLException
+     */
     // REVOKE privilege_name
     // ON object_name
     // FROM {user_name |PUBLIC |role_name}
