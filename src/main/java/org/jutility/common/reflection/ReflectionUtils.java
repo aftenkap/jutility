4eb938f62e51c7533e16342f6a65de813ace8c99
Peter J. Radics <peter.radics@jutility.org>
2014-10-19 15:25:12 -0400

    Debug messages

--- src/main/java/org/jutility/common/reflection/ReflectionUtils.java
+++ src/main/java/org/jutility/common/reflection/ReflectionUtils.java
@@ -1,23 +1,19 @@
 package org.jutility.common.reflection;
 
+
 /*
- * #%L
- * jutility-common
- * %%
- * Copyright (C) 2013 - 2014 jutility.org
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * #%L jutility-common %% Copyright (C) 2013 - 2014 jutility.org %% Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. #L%
  */
 
 
@@ -42,6 +38,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 
 
 /**
@@ -54,6 +52,7 @@ import java.util.Set;
 public class ReflectionUtils {
 
 
+    private static Logger LOG = Logger.getLogger(ReflectionUtils.class);
 
     /**
      * Checks whether the provided {@link Field} has the provided {@link Class
@@ -535,6 +534,7 @@ public class ReflectionUtils {
         }
         catch (ReflectionException e) {
 
+            LOG.debug("Is not a method " + methodOrPropertyName, e);
             return ReflectionUtils.getAccessor(type, methodOrPropertyName);
         }
     }
@@ -915,7 +915,8 @@ public class ReflectionUtils {
         }
         catch (ReflectionException e) {
 
-            // nothing to do
+            LOG.debug("Couldn't get Methor or accessor with name "
+                    + fieldOrAccessorOrMethod, e);
         }
 
         try {
@@ -926,7 +927,9 @@ public class ReflectionUtils {
         }
         catch (ReflectionException e) {
 
-            // nothing to do
+            LOG.debug(
+                    "Couldn't get Field with name " + fieldOrAccessorOrMethod,
+                    e);
         }
 
 
