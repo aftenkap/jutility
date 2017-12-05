c1c90e20177781fe79313a77470479ddb249c2fd
Peter J. Radics <pjradics@gmail.com>
2017-12-04 22:51:24 -0500

    Added missing exception back to IConverter.

--- jutility-io/src/main/java/org/jutility/io/IConverter.java
+++ jutility-io/src/main/java/org/jutility/io/IConverter.java
@@ -1,6 +1,5 @@
 package org.jutility.io;
 
-
 // @formatter:off
 /*
  * #%L
@@ -25,47 +24,47 @@ package org.jutility.io;
 
 /**
  * The {@code IConverter} interface provides a contract for converters.
- * 
+ *
  * @author Peter J. Radics
  * @version 0.1.2
  * @since 0.1.0
  */
 public interface IConverter {
 
-
-
     /**
      * Determines whether or not the {@link IConverter converter} supports
      * conversion from the {@link Class source type} to a given {@link Class
      * target type}.
-     * 
+     *
      * @param sourceType
-     *            the {@link Class source type}.
+     *         the {@link Class source type}.
      * @param targetType
-     *            the {@link Class target type}.
+     *         the {@link Class target type}.
+     *
      * @return {@code true} if the converter supports the conversion;
-     *         {@code false} otherwise.
+     * {@code false} otherwise.
      */
     boolean supportsConversion(final Class<?> sourceType,
             final Class<?> targetType);
 
-
     /**
      * Converts the provided object into an instance of the desired return type.
-     * 
+     *
      * @param <T>
-     *            the document type.
+     *         the document type.
      * @param <S>
-     *            the return type.
-     * 
+     *         the return type.
      * @param documentToConvert
-     *            the document to be converted.
+     *         the document to be converted.
      * @param returnType
-     *            the desired return type.
+     *         the desired return type.
+     *
      * @return a document of the desired format.
+     *
      * @throws ConversionException
-     *             if the return type is not supported or conversion fails.
+     *         if the return type is not supported or conversion fails.
      */
     <T, S> S convert(final T documentToConvert,
-            final Class<? extends S> returnType);
+            final Class<? extends S> returnType)
+            throws ConversionException;
 }
